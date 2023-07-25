cc305c4e8612d24ba7e9faae6e8a6075c4557476
Faizan Abbas <faizan.abbas@nutech.edu.pk>
2023-07-25 13:08:32 +0500

    password init

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 // ignore: must_be_immutable
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final nameController = TextEditingController();
   final cnicController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool passwordText = true;
+
   List<String> dropdownGenderList = [
     'Male',
     'Female',
@@ -21,14 +29,6 @@ class LoginPage extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              const Text(
-                'Personal Information',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff8B010B),
-                ),
-              ),
               Text(
                 'Personal Information',
                 style: TextStyle(
@@ -40,6 +40,7 @@ class LoginPage extends StatelessWidget {
               TextField(
                 controller: nameController,
                 decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person),
                   hintText: 'Type your name here',
                 ),
                 keyboardType: TextInputType.name,
@@ -47,9 +48,19 @@ class LoginPage extends StatelessWidget {
               TextFormField(
                 controller: cnicController,
                 decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.fingerprint),
                   hintText: 'Type 12345-12345-6',
                 ),
                 keyboardType: TextInputType.number,
+              ),
+              TextFormField(
+                controller: passwordController,
+                obscureText: passwordText,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.key),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                  hintText: '********',
+                ),
               ),
               DropdownButton(
                 items: dropdownGenderList
@@ -65,7 +76,11 @@ class LoginPage extends StatelessWidget {
                 onChanged: (value) {},
               ),
               FilledButton(
-                onPressed: () {},
+                onPressed: () {
+                  passwordText = !passwordText;
+                  print('object fffffffffff $passwordText');
+                  setState(() {});
+                },
                 child: const Text('Save'),
               ),
             ],
